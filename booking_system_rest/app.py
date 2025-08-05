5d0d708a94ed4b82f851910aa074307987d71ca3
Sergio GAMA <sergiogama@hotmail.com>
2025-08-05 09:50:00 -0300

    Update app.py
    Incuido o status do FasAPI

--- booking_system_rest/app.py
+++ booking_system_rest/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from models import User, Flight, Booking
 from db import get_db, init_db
